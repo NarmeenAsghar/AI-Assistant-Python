055302f0842365a3692cc069e102d130edcaa630
NarmeenAsghar <163905451+NarmeenAsghar@users.noreply.github.com>
2025-02-12 15:25:29 +0500

    Add files via upload

---
+++ Day-1/hello_world.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+message = "Welcome to the AI Assitant python"
+print(message)
+
+quote = 'An AI assistant built with "PYTHON" is like a trusted guide, turning complex code into simple solutions, always ready to automate and assist with precision and speed'
+print(quote)
